943e0ab9e8cf4cbd489ae0b9d5e55e7f7f7fd26b
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-03-30 15:42:31 +0200

    Initial Commit

---
+++ MapRoute/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapRoute
+//
+//  Created by Кирилл Тараско on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
